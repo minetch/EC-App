8e5497dfe80e00edfe7c272028a62226ceb9f542
ymine,mathematics@gmail.com <ymine.mathematics@gmail.com>
2022-02-21 02:18:24 +0900

    update for redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import createStore from './reducks/store/store';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+export const store = createStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>,
+  document.getElementById('root'),
+)
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
