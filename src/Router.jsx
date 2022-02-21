6279c5771d6a028b5d587af5ad89822d031083a0
ymine,mathematics@gmail.com <ymine.mathematics@gmail.com>
2022-02-21 20:06:00 +0900

    update json for reselect

---
+++ src/Router.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Switch } from 'react-router'
+import { Login, Top, ItemList, ItemView } from './Page'
+
+const Router = () => {
+    return(
+        <Switch>
+            <Route exact path={"(/)?"} component={ Top }/>{/*完全一致*/}
+            <Route exact path={"/login"} component={ Login }/>
+            <Route exact path={"/ItemList"} component={ ItemList }/>
+            <Route path={"/ItemView"} component={ ItemView }/>
+        </Switch>
+    )
+}
+
+export default Router;
