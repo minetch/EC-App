8107e7e8d3adda4184e540553ded521736b02c9e
ymine,mathematics@gmail.com <ymine.mathematics@gmail.com>
2022-02-21 23:20:31 +0900

    Login page

--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,7 +1,11 @@
+import { useDispatch } from "react-redux";
+import { push } from 'connected-react-router'
+
 const Login = () => {
+    const dispatch = useDispatch();
     return(
         <>
-        
+            <button onClick={ () => dispatch(push('/'))}>トップへ</button>
         </>
     );
 }
