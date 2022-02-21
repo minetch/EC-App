c4f0c64e80982510af305ea3124de7647eb59c24
ymine,mathematics@gmail.com <ymine.mathematics@gmail.com>
2022-02-21 20:09:34 +0900

    update for Router

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,16 +1,20 @@
 import {
     createStore as reduxCreateStore,
     combineReducers,
-} from 'redux'
-import { ProductsReducer } from '../products/reducers'
-import { UsersReducer } from '../user/reducers'
+    applyMiddleware
+} from 'redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+//import { ProductsReducer } from '../products/reducers';
+import { UsersReducer } from '../user/reducers';
 
-export default function createStore() {
+export default function createStore(history) {
     return reduxCreateStore(
-
         combineReducers({
-            products:ProductsReducer,
+            router:connectRouter(history),
             users:UsersReducer,
-        })
-    )
+        }),
+        applyMiddleware(
+            routerMiddleware(history)
+        )
+    );
 }
