a1603854a14a4a8788e76272bcbd344ebeb1f740
aibahizuki <45566778+Hizuki1030@users.noreply.github.com>
2022-02-22 23:57:37 +0900

    3dprinterの動作を追加

--- src/Page/ItemView.jsx
+++ src/Page/ItemView.jsx
@@ -1,9 +1,149 @@
+import React, { useCallback, useState , useEffect} from 'react'
+import { TextArea } from '../UI_component';
+import { Button } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/Input';
+
 const ItemView = () => {
+    useEffect(() => {
+        // イベントリスナーを登録
+        var obj1 = document.getElementById("gcode_file");
+        obj1.addEventListener("change",function(evt){
+  
+          var file = evt.target.files;
+  
+          //FileReaderの作成
+          var reader = new FileReader();
+          //テキスト形式で読み込む
+          reader.readAsText(file[0]);
+          
+          //読込終了後の処理
+          reader.onload = function(ev){
+            //テキストエリアに表示する
+          SaveasGcode(reader.result);
+          }
+        },false);
+      }, []);
+
     return(
         <>
-        
+            <div>
+                <Button variant="contained" onClick={PrinterSetting}>購入</Button>                
+                <Paper elevation="6"  className="controll-paper">
+                <text>Gcodeファイル</text>
+                <TextField type = "file" id="gcode_file" label="Standard" variant="standard" />
+                <Button  onClick={SendGcode_fromFile} variant="contained">開始</Button>
+
+              </Paper>
+            </div>
         </>
     );
 }
 
 export default ItemView;
+
+
+
+
+let Gcodes = new Array();
+let Gcode_readed_num;
+let port;
+async function PrinterSetting() {
+    console.log("from onstart")
+      try {
+          port = await navigator.serial.requestPort();
+          await port.open({ baudRate: 115200 });
+          console.log("接続");
+          onConnectButtonClick()
+      } catch (e) {
+          console.log("Error");
+      }
+  }
+
+
+let SerialBuffer;
+async function onConnectButtonClick() {
+  try {
+      while (port.readable) {
+          const reader = port.readable.getReader();
+
+          try {
+              while (true) {
+                  let inputValue="";
+                  while(true){
+                    const { value, done } = await reader.read();
+                    let DecodedValue = new TextDecoder().decode(value)
+                    if (done) {
+                        addSerial("Canceled\n");
+                        break;
+                    }
+                    console.log(DecodedValue)
+                    inputValue = inputValue + DecodedValue
+                    if(inputValue.includes("\n")){
+                      break;
+                    }
+                  }
+                  
+                  addSerial(inputValue)
+                  if(inputValue.includes("\n")){
+                    if(inputValue.includes("ok") || inputValue =="wait\n"){
+                      console.log("sendNextGcode")
+                      SendGcode_fromFile()
+                    }                    
+                  }
+
+
+              }
+          } catch (error) {
+              console.log("Error: Read" + error + "\n");
+          } finally {
+              reader.releaseLock();
+          }
+      }
+  } catch (error) {
+      console.log("Error: Open" + error + "\n");
+  }
+}
+
+function addSerial(text){
+  let monitor = document.getElementById('monitor');
+  console.log(text)
+}
+
+
+function remove_setting(){
+  Gcodes.forEach(function(element, index){
+    if(element[0] ==";"){
+      delete Gcodes[index]
+    }
+  });
+  Gcodes = Gcodes.filter(Boolean)
+}
+
+function SendGcode_fromFile(){
+    let Gcode = Gcodes.shift();
+    console.log(Gcode)
+  
+    writeText(Gcode)
+
+  }
+
+
+  async function writeText(text) {
+      const encoder = new TextEncoder();
+      const writer = port.writable.getWriter();
+      await writer.write(encoder.encode(text+"\n"));
+      console.log("テキスト書き込み: " + text);
+      writer.releaseLock();
+  }
+  
+
+  function SaveasGcode(file_text){
+    let fileContentArray = file_text.split(/\r\n|\n/);
+    Gcodes = Gcodes.concat(fileContentArray)
+    remove_setting()
+    console.log(Gcodes)
+  }
+  
