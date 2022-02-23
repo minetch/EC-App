1e45a6969d6b99341d541956e30caa7974fd25a0
ymine,mathematics@gmail.com <ymine.mathematics@gmail.com>
2022-02-23 15:35:13 +0900

    ItemList

--- src/Page/ItemList.jsx
+++ src/Page/ItemList.jsx
@@ -1,6 +1,13 @@
+import React from 'react'
+import { ItemBox } from '../UI_component';
+
 const ItemList = () => {
     return(
         <>
+            <ItemBox 
+                Price={"1￥"}
+
+            />
             
         </>
     );
