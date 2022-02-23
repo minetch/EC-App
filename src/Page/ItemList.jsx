5000b52541c9276acdd63433fc9c0ecdc58db50d
ymine,mathematics@gmail.com <ymine.mathematics@gmail.com>
2022-02-23 20:25:20 +0900

    edit itemlist

--- src/Page/ItemList.jsx
+++ src/Page/ItemList.jsx
@@ -1,13 +1,86 @@
+import { makeStyles } from '@mui/styles';
 import React from 'react'
 import { ItemBox } from '../UI_component';
+import {coffee_cup, Case} from '../Image'
+import '../reset.css'
+
+const useStyles = makeStyles({
+    group:{
+        display: "flex",
+        margin:"20px 20px",
+    }
+})
 
 const ItemList = () => {
+    const classes = useStyles();
     return(
         <>
-            <ItemBox
-                Price={"¥100"}
-                Maker={"Aiba Hizuki"}
-            />
+            <div className={classes.group}>
+                <ItemBox 
+                    Id={"/ItemView"}
+                    Image={coffee_cup}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />
+                <ItemBox 
+                    Image={Case}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />
+                <ItemBox 
+                    Image={Case}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />  
+                <ItemBox 
+                    Image={Case}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />
+                <ItemBox 
+                    Image={Case}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />
+            </div>
+            <div className={classes.group}>
+                <ItemBox 
+                    Id={"/Top"}
+                    Image={coffee_cup}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />
+                <ItemBox 
+                    Image={Case}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />
+                <ItemBox 
+                    Image={Case}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />  
+                <ItemBox 
+                    Image={Case}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />
+                <ItemBox 
+                    Image={Case}
+                    Name={"Cup"}
+                    Price={"¥100"}
+                    Maker={"Aiba Hizuki"}
+                />
+            </div>
         </>   
     );
 }
