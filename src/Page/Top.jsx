f8a737079bbc5c7b7e123f959bea45e39ab9ab9c
ymine,mathematics@gmail.com <ymine.mathematics@gmail.com>
2022-02-23 20:24:57 +0900

    changed button for mui

--- src/Page/Top.jsx
+++ src/Page/Top.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { push } from 'connected-react-router';
+import { Button } from '@mui/material';
 
 const Top = () => {
     const dispatch = useDispatch();
@@ -9,9 +10,21 @@ const Top = () => {
             <div className="Top-Container">
                 <div className="Top">
                     <h1 className="Top-Title">ShopRint</h1>
-                    <button className="" onClick={() => dispatch(push('/login'))}>ログイン</button>
-                    <button className="" onClick={() => dispatch(push('/ItemList'))}>商品を見る</button>
-                    <button className="" onClick={() => dispatch(push('/AddProducts'))}>商品をアップロード</button>
+                    <Button 
+                        className="" 
+                        onClick={() => dispatch(push('/login'))}
+                        variant="contained"    
+                    >ログイン</Button>
+                    <Button 
+                        className="" 
+                        onClick={() => dispatch(push('/ItemList'))}
+                        variant="contained"
+                    >商品を見る</Button>
+                    <Button 
+                        className="" 
+                        onClick={() => dispatch(push('/AddProducts'))}
+                        variant="contained"
+                    >商品をアップロード</Button>
                 </div>
             </div>
         </>
