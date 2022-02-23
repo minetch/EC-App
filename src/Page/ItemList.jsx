2d9b7b36c1b6a59202e73369319e6f3cb3ba6ff7
ymine,mathematics@gmail.com <ymine.mathematics@gmail.com>
2022-02-23 22:29:07 +0900

    preload image

--- src/Page/ItemList.jsx
+++ src/Page/ItemList.jsx
@@ -1,8 +1,15 @@
 import { makeStyles } from '@mui/styles';
 import React from 'react'
 import { ItemBox } from '../UI_component';
-import {coffee_cup, Case} from '../Image'
+import { coffee_cup, Case, cube, hunger, piece_of_cake, S, toothbrush} from '../Image'
 import '../reset.css'
+import { useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
+
+
+const img = new Image();
+const src= "src/Image"
+img.src = src
 
 const useStyles = makeStyles({
     group:{
@@ -13,72 +20,60 @@ const useStyles = makeStyles({
 
 const ItemList = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
     return(
         <>
             <div className={classes.group}>
                 <ItemBox 
-                    Id={"/ItemView"}
                     Image={coffee_cup}
                     Name={"Cup"}
                     Price={"¥100"}
                     Maker={"Aiba Hizuki"}
+                    onClick={() => dispatch(push('/ItemView/coffee_cup'))}
                 />
                 <ItemBox 
                     Image={Case}
-                    Name={"Cup"}
+                    Name={"謎のケース"}
                     Price={"¥100"}
                     Maker={"Aiba Hizuki"}
+                    onClick={() => dispatch(push('/ItemView/Case'))}
                 />
                 <ItemBox 
-                    Image={Case}
-                    Name={"Cup"}
+                    Image={cube}
+                    Name={"立方体"}
                     Price={"¥100"}
                     Maker={"Aiba Hizuki"}
+                    onClick={() => dispatch(push('/ItemView/cube'))}
                 />  
                 <ItemBox 
-                    Image={Case}
-                    Name={"Cup"}
+                    Image={hunger}
+                    Name={"ハンガー"}
                     Price={"¥100"}
                     Maker={"Aiba Hizuki"}
+                    onClick={() => dispatch(push('/ItemView/hunger'))}
                 />
                 <ItemBox 
-                    Image={Case}
-                    Name={"Cup"}
+                    Image={piece_of_cake}
+                    Name={"組み立てられるケーキ"}
                     Price={"¥100"}
                     Maker={"Aiba Hizuki"}
+                    onClick={() => dispatch(push('/ItemView/piece_of_cake'))}
                 />
             </div>
             <div className={classes.group}>
                 <ItemBox 
-                    Id={"/Top"}
-                    Image={coffee_cup}
-                    Name={"Cup"}
+                    Image={S}
+                    Name={"S字フック"}
                     Price={"¥100"}
                     Maker={"Aiba Hizuki"}
+                    onClick={() => dispatch(push('/ItemView/S'))}
                 />
                 <ItemBox 
-                    Image={Case}
-                    Name={"Cup"}
+                    Image={toothbrush}
+                    Name={"歯ブラシ立て"}
                     Price={"¥100"}
                     Maker={"Aiba Hizuki"}
-                />
-                <ItemBox 
-                    Image={Case}
-                    Name={"Cup"}
-                    Price={"¥100"}
-                    Maker={"Aiba Hizuki"}
-                />  
-                <ItemBox 
-                    Image={Case}
-                    Name={"Cup"}
-                    Price={"¥100"}
-                    Maker={"Aiba Hizuki"}
-                />
-                <ItemBox 
-                    Image={Case}
-                    Name={"Cup"}
-                    Price={"¥100"}
-                    Maker={"Aiba Hizuki"}
+                    onClick={() => dispatch(push('/ItemView/toothbrush'))}
                 />
             </div>
         </>   
