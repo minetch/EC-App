318365b63c45294229f508a27470f029959be097
ymine,mathematics@gmail.com <ymine.mathematics@gmail.com>
2022-02-23 20:23:59 +0900

    edit header footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { makeStyles } from '@mui/styles';
+import React from 'react';
+import '../reset.css'
 
+const useStyles = makeStyles({
+    container:{
+        height:300,
+        backgroundColor:'#202020',
+    },text:{
+        fontSize:100,
+        color:"#B2B2B2",
+        textAlign:"center",
+        padding:"90px",
+        fontWeight:"bold",
+    }
+})
 const Footer = () => {
+    const classes = useStyles();
     return(
         <>
+            <div className={classes.container}>
+                <p className={classes.text}>フッターっぽいもの</p>
+            </div>
         </>
     );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,27 @@
+import { makeStyles } from '@mui/styles';
 import React from 'react'
+import '../reset.css'
 
+const useStyles = makeStyles({
+    container:{
+        height:70,
+        backgroundColor:"#6D9EEB",
+    },title:{
+        fontSize:20,
+        padding:"25px 30px",
+        fontWeight:"bold",
+        color:"white",
+
+    }
+})
 const Header = () => {
+    const classes = useStyles();
     return(
         <>
-        
+            <div className={classes.container}>
+                <p className={classes.title}>ShoPrint</p>
+            </div>
+            
         </>
     );
 }
